7a5c69a2d51659f2b439e212c0c0ba2fd62d650d
Lucy20209060 <550502661@qq.com>
2017-12-26 21:05:30 +0800

    body中加入内容

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,22 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, egg';
+    this.ctx.body = [
+      '<a download href="/download">download</a>',
+      '<br>',
+      '<a download href="/download-image">download image</a>',
+    ].join('');
+  }
+
+  async downloadImage() {
+    console.log(11111)
+    const url = 'http://cdn2.ettoday.net/images/1200/1200526.jpg';
+    const res = await this.ctx.curl(url, {
+      streaming: true,
+    });
+
+    this.ctx.type = 'jpg';
+    this.ctx.body = res.res;
   }
 }
 
